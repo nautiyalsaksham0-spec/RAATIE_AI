173ddddfb45774e90b34734097eaaf73181e58a5
Saksham Nautiyal <nautiyalsaksham0@gmail.com>
2026-06-06 18:10:26 +0530

    did the target speed,evaluation its threat and implemented on screen

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ Colour_drone=(0,255,150)
 Colour_thrust=(255,150,0)
 Colour_text=(255, 255, 255) 
 Colour_alert=(255, 50, 50)
+Colour_target=(255,215,0)
+Colour_vector=(0,180,255)
 class tacticalengine:
     def __init__(self):
         pygame.font.init()
@@ -30,6 +32,13 @@ class tacticalengine:
             {"x": 640,  "y": 550, "radius": 130}   
         ]
         self.active_risk_per=0.0
+        self.target_x = 100
+        self.target_y = 100
+        self.target_speed_x = 2.0 
+        self.target_speed_y = 1.5
+        self.target_speed_inkmh=1200
+        self.target_rcs_size = 5.5
+        self.target_priority = "LOW"
     def cal_threat(self):
         max_risk=0.0
         for radar in self.radar_zones:
@@ -39,6 +48,20 @@ class tacticalengine:
                if current_risk>max_risk:
                     max_risk=current_risk
         return round(max_risk,1)      
+    def evaluate_target(self,distance_to_target):
+           if distance_to_target <300 or self.target_speed_inkmh > 2000:
+              return"CRITICAL THREAT"
+           elif distance_to_target < 600 and self.target_rcs_size > 2.0:
+              return"TACTICAL ENGANGEMENT"
+           else:
+            return"LOW PRIORITY"  
+    def update_target_kinematics(self):
+        self.target_x += self.target_speed_x
+        self.target_y += self.target_speed_y  
+        if self.target_x <= 30 or self.target_x>=Window_width-30:
+            self.target_speed_x *= -1  
+        if self.target_y <= 30 or self.target_y>=Window_height-30:
+            self.target_speed_y *= -1      
     def run(self):
             while self.is_running:
                 for event in pygame.event.get():
@@ -65,6 +88,9 @@ class tacticalengine:
                 self.drone_x=max(20,min(self.drone_x,Window_width-20))
                 self.drone_y=max(20,min(self.drone_y,Window_height-20))
                 self.active_risk_per=self.cal_threat()
+                self.update_target_kinematics()
+                distance_to_target = math.sqrt((self.drone_x - self.target_x)**2 + (self.drone_y - self.target_y)**2)
+                self.target_priority=self.evaluate_target(distance_to_target)
                 self.screen.fill(Colour_bg)
                 for x in range(0,Window_width,Grid_size):
                     pygame.draw.line(self.screen,Colour_grid,(x,0),(x,Window_height),1)
@@ -77,6 +103,14 @@ class tacticalengine:
                     pygame.draw.circle(radar_sur, (255, 50, 50, 120), (radar["radius"], radar["radius"]), radar["radius"], 1)
                     self.screen.blit(radar_sur, (radar["x"] - radar["radius"], radar["y"] - radar["radius"]))
                     pygame.draw.circle(self.screen,Colour_alert,(radar["x"],radar["y"]),3)
+                pygame.draw.line(self.screen,Colour_vector,(self.drone_x,self.drone_y),(self.target_x,self.target_y),1)
+                target_diamond_points = [
+                (self.target_x, self.target_y - 12),
+                (self.target_x + 12, self.target_y),  
+                (self.target_x, self.target_y + 12),  
+                (self.target_x - 12, self.target_y)   ]
+                pygame.draw.polygon(self.screen, Colour_target, target_diamond_points, 2)
+                pygame.draw.rect(self.screen, Colour_alert, (self.target_x - 3, self.target_y - 3, 6, 6))
                 if is_accelerating:
                    thrust_points=[
                     (self.drone_x-15,self.drone_y+10),
@@ -100,15 +134,25 @@ class tacticalengine:
                 pygame.draw.rect(self.screen, Colour_grid, (15, 15, 300, 100), 1)
                 txtpos=f"Ucav Coords x:[{int (self.drone_x)}] y:[{int(self.drone_y)}]"
                 txtrisk=f"Threat level:{self.active_risk_per}%"
-                if self.active_risk_per > 0:
+                txt_tgt_dist=f"TARGET DISTANCE:{int(distance_to_target)}px"
+                txt_targt=f"TARGET EVALUATION:{self.target_priority}"
+                if self.active_risk_per>0:
                  txt_status = "SYSTEM STATUS: WARNING - RADAR LOCK DETECTED"
                  status_color = Colour_alert
                 else:
                  txt_status = "SYSTEM STATUS: AIRSPACE CLEAR"
                  status_color = Colour_drone
+                if self.target_priority=="CRITICAL THREAT":
+                 priority_color = Colour_alert
+                elif self.target_priority=="TACTICAL ENGAGEMENT":
+                 priority_color = Colour_target
+                else:
+                 priority_color = Colour_vector
                 self.screen.blit(self.font.render(txtpos, True, Colour_text), (25, 25))
                 self.screen.blit(self.font.render(txtrisk, True, status_color), (25, 50))
                 self.screen.blit(self.font.render(txt_status, True, status_color), (25, 75))
+                self.screen.blit(self.font.render(txt_tgt_dist, True, Colour_text), (25, 100))
+                self.screen.blit(self.font.render(txt_targt, True, priority_color), (25, 125))
                 pygame.display.flip()
                 self.clock.tick(60)
             pygame.quit()
